5f5fbffb56c024a898cabee6844657d2a26977c9
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-04-09 23:31:13 +0800

    Add more specs for Numeric Cnpjs

--- spec/br-cnpj_spec.rb
+++ spec/br-cnpj_spec.rb
@@ -1,7 +1,21 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
-describe "BrCnpj" do
-  it "fails" do
-    fail "No specs defined yet"
+describe BR::CNPJ do
+      
+  context "when integer" do
+
+    it "should return true for valid cnpjs" do
+      VALID_CNPJS_INTEGER.each do |valid_cnpj|
+        BR::CNPJ.valid?(valid_cnpj).should be_true
+      end
+    end
+    
+    it "should return false for invalid cnpjs" do
+      INVALID_CNPJS_INTEGER.each do |invalid_cnpj|
+        BR::CNPJ.valid?(invalid_cnpj).should be_false
+      end
+    end
+    
   end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-require 'spec'
-require 'spec/autorun'
+require File.join(File.dirname(__FILE__), '../lib/br/cnpj')
+require File.join(File.dirname(__FILE__), 'faker_cnpj')
 
 Spec::Runner.configure do |config|
   

fefc1ee216201d8a9e5f11c0f02ac860037f5cc6
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-04-10 09:47:54 +0800

    Adicionado spec para Cnpjs em String
    - Tirado Runner desnecessário do Rspec

--- spec/br-cnpj_spec.rb
+++ spec/br-cnpj_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe BR::CNPJ do
       
-  context "when integer" do
+  context "when Fixnum" do
 
     it "should return true for valid cnpjs" do
       VALID_CNPJS_INTEGER.each do |valid_cnpj|
@@ -12,6 +12,22 @@ describe BR::CNPJ do
     
     it "should return false for invalid cnpjs" do
       INVALID_CNPJS_INTEGER.each do |invalid_cnpj|
+        BR::CNPJ.valid?(invalid_cnpj).should be_false
+      end
+    end
+    
+  end
+  
+  context "when String" do
+    
+    it "should return true for valid cnpjs" do
+      VALID_CNPJS_STRING.each do |valid_cnpj|
+        BR::CNPJ.valid?(valid_cnpj).should be_true
+      end
+    end
+    
+    it "should return false for invalid cnpjs" do
+      INVALID_CNPJS_STRING.each do |invalid_cnpj|
         BR::CNPJ.valid?(invalid_cnpj).should be_false
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,2 @@
 require File.join(File.dirname(__FILE__), '../lib/br/cnpj')
 require File.join(File.dirname(__FILE__), 'faker_cnpj')
-
-Spec::Runner.configure do |config|
-  
-end

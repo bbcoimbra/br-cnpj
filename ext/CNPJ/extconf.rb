a2bf41a9d483f87a05d25fc6a14f20a76b53c7b4
Bruno Coimbra <bbcoimbra@gmail.com>
2010-04-16 16:26:21 -0300

    fix issue 1

--- ext/CNPJ/extconf.rb
+++ ext/CNPJ/extconf.rb
@@ -3,5 +3,8 @@ require 'mkmf'
 dir_config('cnpj')
 
 have_header('stdlib.h')
-
-create_makefile('CNPJ')
+have_func('lldiv')
+have_type('lldiv_t','stdlib.h')
+with_cflags('-std=c99') do
+  create_makefile('CNPJ')
+end

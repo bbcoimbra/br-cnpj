a68a2c60813fa7eef8f11ccd21e3028f742e46fd
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-04-19 05:26:49 +0800

    Add useful methods to CNPJ

--- lib/br/cnpj.rb
+++ lib/br/cnpj.rb
@@ -3,17 +3,46 @@ require File.join(File.dirname(__FILE__), '../CNPJ')
 module BR
   class CNPJ
     class << self
+        
         alias_method :orig_valid?, :valid?
+        
+        # Formata o Cnpj desformatado
+        #
+        # Cnpj.formatar(191) # => '00.000.000/0001-91'
+        # Cnpj.formatar(30553786000135) # => "30.553.786/0001-35"
+        #
+        def format(cnpj)
+          cnpj = cnpj.to_s.rjust(14, "0")
+          "%s.%s.%s/%s-%s" % [cnpj[0,2], cnpj[2,3], cnpj[5,3], cnpj[8,4], cnpj[12,2]]
+        end
+        alias_method :formatar, :format
+        
+        # Desformata o Cnpj formatado
+        #
+        # Cnpj.desformatar("02.716.485/0001-40") # => "02716485000140"
+        # Cnpj.unformat("00.086.001/0001-04") # => "00086001000104"
+        #
+        def unformat(cnpj)
+           cnpj.gsub(/\D/, '')
+        end
+        alias_method :desformatar, :unformat
+        
+        # Retorna true se o Cnpj for válido
+        # Retorna falso se o Cnpj for inválido
+        #
+        # CNPJ.valid?(191)           # => true
+        # CNPJ.valid?(123456789101)
+        #
+        def valid?(cnpj)
+          if cnpj.is_a? String
+            orig_valid?(cnpj.to_i)
+          else
+            orig_valid?(cnpj)
+          end
+        end
+        
     end
-
-    def self.valid?(cnpj)
-      if cnpj.is_a? String
-        orig_valid?(cnpj.to_i)
-      else
-        orig_valid?(cnpj)
-      end
-    end
-
+    
     def to_i
       @raiz * 1_000_000 + @filial * 100 + @verif
     end
@@ -21,5 +50,6 @@ module BR
     def to_s
       "%014d" % to_i
     end
+    
   end
 end
--- spec/br-cnpj_spec.rb
+++ spec/br-cnpj_spec.rb
@@ -1,35 +1,83 @@
 require 'spec_helper'
 
-describe BR::CNPJ do
-      
-  context "when Fixnum" do
-
-    it "should return true for valid cnpjs" do
-      VALID_CNPJS_INTEGER.each do |valid_cnpj|
-        BR::CNPJ.valid?(valid_cnpj).should be_true
-      end
-    end
-    
-    it "should return false for invalid cnpjs" do
-      INVALID_CNPJS_INTEGER.each do |invalid_cnpj|
-        BR::CNPJ.valid?(invalid_cnpj).should be_false
-      end
-    end
-    
-  end
+module BR
   
-  context "when String" do
-    
-    it "should return true for valid cnpjs" do
-      VALID_CNPJS_STRING.each do |valid_cnpj|
-        BR::CNPJ.valid?(valid_cnpj).should be_true
+  describe CNPJ do
+      
+    context "when Fixnum" do
+
+      it "should return true for valid cnpjs" do
+        VALID_CNPJS_INTEGER.each do |valid_cnpj|
+          CNPJ.valid?(valid_cnpj).should be_true
+        end
       end
+    
+      it "should return false for invalid cnpjs" do
+        INVALID_CNPJS_INTEGER.each do |invalid_cnpj|
+          CNPJ.valid?(invalid_cnpj).should be_false
+        end
+      end
+    
+    end
+  
+    context "when String" do
+    
+      it "should return true for valid cnpjs" do
+        VALID_CNPJS_STRING.each do |valid_cnpj|
+          CNPJ.valid?(valid_cnpj).should be_true
+        end
+      end
+    
+      it "should return false for invalid cnpjs" do
+        INVALID_CNPJS_STRING.each do |invalid_cnpj|
+          CNPJ.valid?(invalid_cnpj).should be_false
+        end
+      end
+    
+    end
+  
+    context 'when format cnpj' do
+    
+      it 'should format the cnpj put zero in the left' do
+        CNPJ.format(191).should eql('00.000.000/0001-91')
+      end
+  
+      it 'should format the cnpj put zero in the left (cnpj filial)' do
+        CNPJ.format(272).should  eql('00.000.000/0002-72')
+      end
+    
+      it "should format the cnpj put period and stuff" do
+        CNPJ.formatar(30553786000135).should eql("30.553.786/0001-35")
+      end
+    
+      it "should format the cnpj" do
+        CNPJ.formatar(14712911000156).should eql("14.712.911/0001-56")
+      end
+    
     end
     
-    it "should return false for invalid cnpjs" do
-      INVALID_CNPJS_STRING.each do |invalid_cnpj|
-        BR::CNPJ.valid?(invalid_cnpj).should be_false
+    context 'when unformat cnpj' do
+
+      it "should return only numbers in String" do
+        CNPJ.unformat("02.716.485/0001-40").should == "02716485000140"
       end
+
+      it "should remove the chars '.', '/' and '-'" do
+        CNPJ.unformat("71.153.638/0001-00").should == "71153638000100"
+      end
+
+      it "should remove the chars with '-'" do
+        CNPJ.desformatar("47.580.835/0001-21").should == "47580835000121"
+      end
+
+      it "should remove the chars with '/'" do
+        CNPJ.desformatar("98.819.832/0001-32").should == "98819832000132"
+      end
+
+      it "should retorn an String" do
+        CNPJ.desformatar("00.022.559/0001-13").should be_kind_of(String)
+      end
+
     end
     
   end
